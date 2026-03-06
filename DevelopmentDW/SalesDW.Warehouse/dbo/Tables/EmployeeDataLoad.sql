b0e25046bbf07884912db7dd9933d95c3e28ff4f
MSFabricDemo <Praveen@vgad222.onmicrosoft.com>
2026-03-06 18:52:34 +0530

    New Items Changed

---
+++ DevelopmentDW/SalesDW.Warehouse/dbo/Tables/EmployeeDataLoad.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo].[EmployeeDataLoad] (
+
+	[EmpId] varchar(8000) NULL, 
+	[EmpName] varchar(8000) NULL, 
+	[Salary] varchar(8000) NULL, 
+	[DeptName] varchar(8000) NULL, 
+	[Location] varchar(8000) NULL, 
+	[Country] varchar(8000) NULL
+);
