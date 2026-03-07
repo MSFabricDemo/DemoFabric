349febf2d040a0cd1cb4ea1c7e51563394d85d0b
MSFabricDemo <Praveen@vgad222.onmicrosoft.com>
2026-03-07 11:02:48 +0530

    Notebook Added

---
+++ DevelopmentDW/TestResultNotebook.Notebook/notebook-content.py
@@ -0,0 +1,32 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "e90e9995-11e0-4b0a-9dee-5ce509a3e1f5",
+# META       "default_lakehouse_name": "TestLHData",
+# META       "default_lakehouse_workspace_id": "576e59b5-d362-4bae-9384-e319f3502a1f",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "e90e9995-11e0-4b0a-9dee-5ce509a3e1f5"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+varEmpId = 101
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
